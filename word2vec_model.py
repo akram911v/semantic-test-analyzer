b001fb27bba7a008c31044430919843bc7f503d3
akram911v <akramab9911@gmail.com>
2025-11-03 13:36:30 +0700

    Update word2vec_model.py

--- word2vec_model.py
+++ word2vec_model.py
@@ -39,3 +39,30 @@ class Word2VecModel:
         if vec1 is not None and vec2 is not None:
             return cosine_similarity([vec1], [vec2])[0][0]
         return 0.0
+    def find_similar_documents(self, query_tokens, documents, top_n=5):
+        if not self.model:
+            raise ValueError("Model not trained")
+        
+        query_vector = self._get_document_vector(query_tokens)
+        similarities = []
+        
+        for doc_id, doc_vector in self.document_vectors.items():
+            sim = cosine_similarity([query_vector], [doc_vector])[0][0]
+            similarities.append((doc_id, sim, documents[doc_id]))
+        
+        similarities.sort(key=lambda x: x[1], reverse=True)
+        return similarities[:top_n]
+    
+    def _get_document_vector(self, tokens):
+        word_vectors = []
+        for word in tokens:
+            if word in self.model.wv:
+                word_vectors.append(self.model.wv[word])
+        if word_vectors:
+            return np.mean(word_vectors, axis=0)
+        return np.zeros(self.vector_size)
+    
+    def get_similar_words(self, word, top_n=10):
+        if self.model and word in self.model.wv:
+            return self.model.wv.most_similar(word, topn=top_n)
+        return []
